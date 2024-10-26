69950ccd86c3327ef0860298c7431040c7e5a6e1
ParkSeongGeun <phd0801@naver.com>
2024-10-26 13:57:59 +0900

    [Setting] Project Setting

---
+++ GithubUserTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubUserTracker
+//
+//  Created by 박성근 on 10/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
